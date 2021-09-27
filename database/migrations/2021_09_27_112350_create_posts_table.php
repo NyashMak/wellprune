1a053d98395bb1c7106766403c1af4f4c621ed6e
NyashMak <nyashagmakwavarara@gmail.com>
2021-09-27 13:52:32 +0200

    Created 'Post' model, migrations, controller and seeder. And updated the schema in the migrations file to add extra columns to the Posts table in my MySQL Database

---
+++ database/migrations/2021_09_27_112350_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id('post_id');
+            $table->foreignId('user_id');
+            $table->text('coverimage');
+            $table->text('topic');
+            $table->text('content');
+            $table->text('tags');
+            $table->text('link');
+            $table->timestamps();
+            $table->integer('comments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
