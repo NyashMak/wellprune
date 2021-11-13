35a5857d81385efdcb4d2c75277aba40f17d98eb
NyashMak <nyashagmakwavarara@gmail.com>
2021-11-13 11:34:23 +0200

    updated webpack mix to compile CSS into my app

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -31,6 +31,7 @@ class PostController extends Controller
     public function editpost ($post) {
         // returns view with form to update post $post
         // $postID = $post;
+        // $postID = 
         return view('admin.blog.editpost');
     }
 
--- resources/views/admin/blog/editpost.blade.php
+++ resources/views/admin/blog/editpost.blade.php
@@ -7,13 +7,13 @@
 <div id="editpostform" class="form-group">
     <form action="" method="POST" enctype="multipart/form-data">
         <label for="coverimage">Cover Image</label>
-        <input type="file" id="coverimage" accept="image/*" name="coverimage" class="form-control" value=""><br>
+        <input type="file" id="coverimage" accept="image/*" name="coverimage" class="form-control" placeholder=""><br>
 
         <label for="topic">Topic</label>
-        <input type="text" name="topic" id="topic" class="form-control" value=""><br>
+        <input type="text" name="topic" id="topic" class="form-control" placeholder=""><br>
 
         <label for="content">Content</label>
-        <textarea name="content" id="content" class="form-control" rows="3" value=""></textarea><br>
+        <textarea name="content" id="content" class="form-control" rows="3" placeholder=""></textarea><br>
 
         <label for="tags">Tags</label>
         <input type="text" id="tags" name="tags" class="form-control" value=""><br>
